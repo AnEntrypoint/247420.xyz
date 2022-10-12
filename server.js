a32325f8a732fffca16a5bf2a0a8083172f40912
lanmower <almagestfraternite@gmail.com>
2022-10-12 20:03:10 +0000

    initial

---
+++ server.js
@@ -0,0 +1,13 @@
+require('./index.js')
+
+const express = require('express')
+const app = express()
+const port = 8082
+
+app.get('/', (req, res) => res.redirect('./index.html'))
+
+app.use(express.static('./dist/client'))
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+)
