1a698b0cb26528cf76c458e916125d13486a1b05
lanmower <almagestfraternite@gmail.com>
2022-10-17 17:21:26 +0000

    aframe

---
+++ libs.js
@@ -0,0 +1,50 @@
+import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import { KTX2Loader } from './node_modules/three/examples/jsm/loaders/KTX2Loader.js';
+import { DRACOLoader } from './node_modules/three/examples/jsm/loaders/DRACOLoader.js';
+
+module.exports.component = AFRAME.registerComponent("full-gltf-model", {
+    schema: { type: 'model' },
+    init: function () {
+        console.log(window.MeshoptDecoder);
+        const renderer = this.el.sceneEl.renderer
+        // Instantiate a loader
+        this.loader = new GLTFLoader()
+            .setCrossOrigin('anonymous')
+            .setMeshoptDecoder(window.MeshoptDecoder)
+            .setDRACOLoader(new DRACOLoader().setDecoderPath('./wasm/'))
+            .setKTX2Loader(new KTX2Loader().detectSupport(renderer));
+        this.model = null;
+    },
+    update() {
+        var self = this;
+        var el = this.el;
+        var src = this.data;
+
+        if (!src) { return; }
+        this.remove();
+        // Load a glTF resource
+        this.loader.load(
+            // resource URL
+            src,
+            // called when the resource is loaded
+            function gltfLoaded(gltfModel) {
+                console.log("MODEL LOADED", gltfModel)
+                self.model = gltfModel.scene || gltfModel.scenes[0];
+                self.model.animations = gltfModel.animations;
+                // setObject3D is needs an object3D, and the model is a group?
+                let rootItem = new THREE.Object3D();
+                rootItem.add(self.model)
+                el.setObject3D('mesh', rootItem);
+                el.emit('model-loaded', { format: 'gltf', model: self.model });
+            }, undefined /* onProgress */,
+            function gltfFailed(error) {
+                var message = (error && error.message) ? error.message : 'Failed to load glTF model';
+                console.warn(message);
+                el.emit('model-error', { format: 'gltf', src: src });
+            });
+    },
+    remove: function () {
+        if (!this.model) { return; }
+        this.el.removeObject3D('mesh');
+    }
+});
